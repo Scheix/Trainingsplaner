db2357ab6f3454ecbef48d3660d25f4709ca9e7b
Simon Zarhuber <starhuber@outlook.de>
2016-08-20 09:03:05 +0200

    SQLite (Simon Zarhuber)

--- Trainingsplaner/Trainingsplaner/Form1.cs
+++ Trainingsplaner/Trainingsplaner/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Trainingsplaner
 {
@@ -19,7 +20,19 @@ namespace Trainingsplaner
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //Start
+            SQLiteConnection.CreateFile("Trainingsplaner.sqlite");
+            SQLiteConnection dbConnection;
+            dbConnection =
+            new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            dbConnection.Open();
+
+            string sql_tableCreate = "CREATE TABLE trainingsplaner (kategorie VARCHAR(20), unterkategorie VARCHAR(20), beschreibung VARCHAR(120), kurzbeschreibung VARCHAR(50), dauer INT, bild BLOB)";
+            SQLiteCommand command = new SQLiteCommand(sql_tableCreate, dbConnection);
+            command.ExecuteNonQuery();
+
+            string sql_values = "insert into trainingsplaner (kategorie, unterkategorie, beschreibung, kurzbeschreibung, dauer, bild) values ('Aufwaermen', 'Einlaufen', 'verschiedene Übungen in den Ecken', null, 15, C:\\Users\\Simon\\Desktop\\Schule\\Diplomarbeit\\Projekt\\Trainingsplaner\\Trainingsplaner\\Pictures\\pirelli.png)";
+            SQLiteCommand command1 = new SQLiteCommand(sql_values, dbConnection);
+            command1.ExecuteNonQuery();
         }
     }
 }
