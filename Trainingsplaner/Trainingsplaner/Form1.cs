06712def29cdd51ddc744520219ed4d552de31cb
Simon Zarhuber <starhuber@outlook.de>
2016-08-20 18:19:49 +0200

    --- Simon Zarhuber Laden des Bildes in die PictureBox

--- Trainingsplaner/Trainingsplaner/Form1.cs
+++ Trainingsplaner/Trainingsplaner/Form1.cs
@@ -25,10 +25,10 @@ namespace Trainingsplaner
             SQLiteConnection trainingsDB;
             trainingsDB = new SQLiteConnection("Data Source=Trainingsplaner.sqlite;Version=3;");
             trainingsDB.Open();
-            string create = "create table trainingsplaner (kategorie varchar(20), unterkategorie varchar(20), beschreibung varchar(200), name varchar(20), dauer int, bild blob)";
+            string create = "create table trainingsplaner (kategorie varchar(20), unterkategorie varchar(20), beschreibung varchar(200), name varchar(20), dauer int, bild varchar(100))";
             SQLiteCommand command = new SQLiteCommand(create, trainingsDB);
             command.ExecuteNonQuery();
-            string insert = "insert into trainingsplaner (kategorie, unterkategorie, beschreibung, name, dauer, bild) values ('Aufwaermen','Einlaufen','Verschiedene Übungen in den Ecken','Workout1',15, 'C:\\Users\\Michael\\Desktop\\Eigene Dateien\\Schule\\Diplomarbeit\\Trainingsplaner\\Trainingsplaner\\Trainingsplaner\\Pictures\\pirelli.png')";
+            string insert = "insert into trainingsplaner (kategorie, unterkategorie, beschreibung, name, dauer, bild) values ('Aufwaermen','Einlaufen','Verschiedene Übungen in den Ecken','Workout1',15, 'C:\\Users\\Simon\\Desktop\\Schule\\Diplomarbeit\\Projekt\\Trainingsplaner\\Trainingsplaner\\Pictures\\pirelli.png')";
             command = new SQLiteCommand(insert, trainingsDB);
             command.ExecuteNonQuery();
             string select = "select kategorie, bild from trainingsplaner";
@@ -37,21 +37,20 @@ namespace Trainingsplaner
             while (reader.Read())
             {
                 this.Text = "" + reader["kategorie"];
-                byte[] result = (byte[])reader["bild"];
-                //int arraySize = result.GetUpperBound(0);
-                pictureBox1.Image = convertByteArrayToImage(result);
+                pictureBox1.Image = Image.FromFile("" + reader["bild"]);
+
             }
         }
-        private Image convertByteArrayToImage (byte[] bytes)
-        {
-            Image image = null;
-            using (MemoryStream ms = new MemoryStream(bytes, true))
-            {
-                ms.Position = 0;
-                ms.Write(bytes, 0, bytes.Length);
-                image = Image.FromStream(ms);
-            }
-            return image;
-        }
+        //private Image convertByteArrayToImage (byte[] bytes)
+        //{
+        //    Image image = null;
+        //    using (MemoryStream ms = new MemoryStream(bytes, true))
+        //    {
+        //        ms.Position = 0;
+        //        ms.Write(bytes, 0, bytes.Length);
+        //        image = Image.FromStream(ms);
+        //    }
+        //    return image;
+        //}
     }
 }
