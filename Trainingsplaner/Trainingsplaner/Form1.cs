68f0822028614afb852f6205ee36d35be77f77e2
michael.scheuringer <michael.scheuringer@me.com>
2016-08-21 09:55:09 +0200

    Umstrukturieren des Codes Bessere Aufteilung

--- Trainingsplaner/Trainingsplaner/Form1.cs
+++ Trainingsplaner/Trainingsplaner/Form1.cs
@@ -9,11 +9,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SQLite;
 using System.IO;
+using System.Data.Entity;
 
 namespace Trainingsplaner
 {
     public partial class Form1 : Form
     {
+        SQLiteConnection trainingsDB = new SQLiteConnection("Data Source=Trainingsplaner.sqlite;Version=3;");
         public Form1()
         {
             InitializeComponent();
@@ -21,37 +23,28 @@ namespace Trainingsplaner
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Change the directory of simon und michael to the same folder, because of the import of the pictures
+            initializeDatabase();
+            string select = "select kategorie, bild from trainingsplaner";
+            SQLiteCommand command = new SQLiteCommand(select, trainingsDB);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                this.Text = "" + reader["kategorie"];
+                pictureBox1.Image = Image.FromFile(""+reader["bild"]);
+            }
+            trainingsDB.Close();
+        }
+        private void initializeDatabase ()
+        {
             SQLiteConnection.CreateFile("Trainingsplaner.sqlite");
-            SQLiteConnection trainingsDB;
-            trainingsDB = new SQLiteConnection("Data Source=Trainingsplaner.sqlite;Version=3;");
             trainingsDB.Open();
-            string create = "create table trainingsplaner (kategorie varchar(20), unterkategorie varchar(20), beschreibung varchar(200), name varchar(20), dauer int, bild blob)";
+            string create = "create table trainingsplaner (kategorie varchar(20), unterkategorie varchar(20), beschreibung varchar(200), name varchar(20), dauer int, bild varchar(100))";
             SQLiteCommand command = new SQLiteCommand(create, trainingsDB);
             command.ExecuteNonQuery();
             string insert = "insert into trainingsplaner (kategorie, unterkategorie, beschreibung, name, dauer, bild) values ('Aufwaermen','Einlaufen','Verschiedene Übungen in den Ecken','Workout1',15, 'C:\\Users\\Michael\\Desktop\\Eigene Dateien\\Schule\\Diplomarbeit\\Trainingsplaner\\Trainingsplaner\\Trainingsplaner\\Pictures\\pirelli.png')";
             command = new SQLiteCommand(insert, trainingsDB);
             command.ExecuteNonQuery();
-            string select = "select kategorie, bild from trainingsplaner";
-            command = new SQLiteCommand(select, trainingsDB);
-            SQLiteDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                this.Text = "" + reader["kategorie"];
-                byte[] result = (byte[])reader["bild"];
-                //int arraySize = result.GetUpperBound(0);
-                pictureBox1.Image = convertByteArrayToImage(result);
-            }
-        }
-        private Image convertByteArrayToImage (byte[] bytes)
-        {
-            Image image = null;
-            using (MemoryStream ms = new MemoryStream(bytes, true))
-            {
-                ms.Position = 0;
-                ms.Write(bytes, 0, bytes.Length);
-                image = Image.FromStream(ms);
-            }
-            return image;
         }
     }
 }
