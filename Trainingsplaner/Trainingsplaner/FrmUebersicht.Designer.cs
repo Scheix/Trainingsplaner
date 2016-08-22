3d35f71566515f5f493f1ad42ab37f1a742d0ced
michael.scheuringer <michael.scheuringer@me.com>
2016-08-22 09:51:11 +0200

    Uebungen einfügen und in der Form Uebungs Uebersicht ausgeben

--- Trainingsplaner/Trainingsplaner/FrmUebersicht.Designer.cs
+++ Trainingsplaner/Trainingsplaner/FrmUebersicht.Designer.cs
@@ -28,19 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(259, 236);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // FrmUebersicht
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listView1);
             this.Name = "FrmUebersicht";
             this.Text = "Uebungs-Uebersicht";
+            this.Load += new System.EventHandler(this.FrmUebersicht_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
--- Trainingsplaner/Trainingsplaner/FrmUebersicht.cs
+++ Trainingsplaner/Trainingsplaner/FrmUebersicht.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,23 @@ namespace Trainingsplaner
 {
     public partial class FrmUebersicht : Form
     {
+        SQLiteConnection trainingsDB = new SQLiteConnection("Data Source=Trainingsplaner.sqlite;Version=3;");
         public FrmUebersicht()
         {
             InitializeComponent();
+        }
+
+        private void FrmUebersicht_Load(object sender, EventArgs e)
+        {
+            trainingsDB.Open();
+            string select = "select name from uebungen";
+            SQLiteCommand command = new SQLiteCommand(select, trainingsDB);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                listView1.Items.Add(""+reader["name"]);
+            }
+            trainingsDB.Close();
         }
     }
 }
